53f8411ac503320583b6c38f9a47c298ec47d427
baejeongwan <jayden.bae@outlook.kr>
2022-10-05 21:00:53 +0900

    Initial Commit

---
+++ script.js
@@ -0,0 +1,110 @@
+let aboutModal = new bootstrap.Modal(document.getElementById('aboutModal'))
+let landingModal = new bootstrap.Modal(document.getElementById('landingModal'))
+let videoModal = new bootstrap.Modal(document.getElementById('videoModal'))
+let videoPlayModal = new bootstrap.Modal(document.getElementById('videoPlayModal'))
+let flightInfoModal = new bootstrap.Modal(document.getElementById('flightInfoModal'))
+let imageModal = new bootstrap.Modal(document.getElementById('imageModal'))
+let imageShowModal = new bootstrap.Modal(document.getElementById('imageShowModal'))
+let videoList = []
+let imageList = []
+
+
+//#region 잠금화면 제어
+function unlockUI() {
+    document.getElementById('lockscreen').classList.add('unlocked')
+    setTimeout(() => {
+        document.getElementById('lockscreen').classList.add('d-none')
+        document.getElementById('container').classList.remove('d-none')
+    }, 500)
+}
+
+function lockUI() {
+    document.getElementById('lockscreen').classList.remove('d-none')
+    document.getElementById('lockscreen').classList.remove('unlocked')
+    setTimeout(() => {
+        document.getElementById('container').classList.add('d-none')
+    }, 500)
+    
+}
+//#endregion
+
+//#region 시계
+setInterval(clockUpdate, 500)
+
+function clockUpdate() {
+    let now = new Date();
+    let hour = 0;
+    let minute = 0;
+    if (now.getMinutes() < 10) {
+        minute = "0" + now.getMinutes().toString()
+    } else {
+        minute = now.getMinutes().toString()
+    }
+
+    if (now.getHours() < 10) {
+        hour = "0" + now.getHours().toString()
+    } else {
+        hour = now.getHours().toString()
+    }
+
+    document.getElementById('clock').innerText = hour + " : " + minute
+    console.log(hour + " : " + minute);
+}
+//#endregion
+
+//#region 버튼 작업
+function about() {
+    aboutModal.show()
+}
+
+function land() {
+    landingModal.show()
+}
+
+function video() {
+    videoModal.show()
+    fetch('./video/database.json')
+        .then((response) => response.json())
+        .then((data) => videoList = data)
+        .then(() => {
+            document.getElementById('videoListGroup').innerHTML = ""
+            videoList.forEach((element, index) => {
+                document.getElementById('videoListGroup').innerHTML += `
+                <a onclick="playVideo(${index})" class="list-group-item list-group-item-action">${element.file}</a>`
+            })
+        })
+}
+
+function image() {
+    imageModal.show()
+    fetch('./image/database.json')
+        .then((response) => response.json())
+        .then((data) => imageList = data)
+        .then(() => {
+            document.getElementById('imageListGroup').innerHTML = ""
+            imageList.forEach((element, index) => {
+                document.getElementById('imageListGroup').innerHTML += `
+                <a onclick="showImage(${index})" class="list-group-item list-group-item-action">${element.file}</a>`
+            })
+        })
+}
+
+function flightInfo() {
+    flightInfoModal.show()
+}
+//#endregion
+
+//#region 미디어
+function playVideo(number) {
+    videoPlayModal.show()
+    document.getElementById('videoPlayModalLabel').innerText = videoList[number].file
+    document.getElementById('videoPlayModalvideo').src = "./video/" + videoList[number].file
+}
+
+function showImage(number) {
+    imageShowModal.show()
+    document.getElementById('imageShowModalLabel').innerText = imageList[number].file
+    document.getElementById('imageShowModalImage').src = "./image/" + imageList[number].file
+}
+
+//#endregion
